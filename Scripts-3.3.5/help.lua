118986b52c36b377e70d3b595461ce62b8194122
Krigsgaldrnet <simon.klaus@gmail.com>
2025-03-09 00:03:11 +0100

    Update. Now working with 3.3.5a

--- Scripts-3.3.5/help.lua
+++ Scripts-3.3.5/help.lua
@@ -1,10 +1,27 @@
-function Buff_Command(event, player, message)
-if (message == "#help") then
-player:FullCastSpell(54675)
-player:FullCastSpell(20217)
-player:FullCastSpell(58451)
-player:FullCastSpell(48161)
-player:SendBroadcastMessage("God helped you!")
+--Status: Working at 3.3.5
+
+-- by Marko at getmangos
+
+--You can use this simple script and add any spells as buffs you want. Can also change here whatever broadcast message you want when player gets buffed, and can even change the default .buff command to something else. 
+
+
+function buff(event, player, message, Type, lang)
+if(message:lower() == "buff")then
+player:AddAura(48074, player)
+player:AddAura(35912, player)
+player:AddAura(38734, player)
+player:AddAura(65075, player)
+player:AddAura(65077, player)
+player:AddAura(43223, player)
+player:AddAura(15366, player)
+player:AddAura(36880, player)
+player:AddAura(36880, player)
+player:AddAura(16609, player)
+player:AddAura(48073, player)
+player:AddAura(48469, player)
+player:AddAura(26035, player)
+player:SendBroadcastMessage("You have been Buffed!")
 end
 end
-RegisterServerHook(16, "Buff_Command")
+
+RegisterPlayerEvent(42, buff)
